f05d38ba07bb902f9b49f661cd2c82f6e3a8690f
Evgeniy <alekssyvorov29@gmail.com>
2023-04-26 21:58:18 +0300

    V 1.0

--- app.py
+++ app.py
@@ -4,25 +4,36 @@ from random import randint
 app = Flask(__name__)
 
 
+@app.route('/')
+def get_lst():
+    return f'{constant}'
+
+
+@app.route('/post')
+def post_lst():
+    constant.append(randint(-100, 100))
+    return f'{constant}'
+
+
+@app.route('/dell')
+def dell_lst():
+    constant.pop()
+    return f'{constant}'
+
+
+def create_lst():
+    from random import randint
+    for i in range(10):
+        constant.append(randint(-50, 50))
+    return constant
+
+
+# Дальше уже пытаюсь сам что-то писать
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home', methods=['POST', 'GET'])
 def index():
     # Для выполнения ДЗ, потом удалю
-    global constant
-    for i in range(randint(1, 20)):
-        constant.append(randint(-50, 50))
     return render_template('index.html', constant=constant)
-
-@app.route('/hw2')
-def hwDelete():
-    '''
-    Функция удаляет со списка элемент и возвращает его, передает в рендер шаблона, где он выводится
-    :return: удаленный элемент списка
-    '''
-    global constant
-    if len(constant):
-        elem = constant.pop()
-    return render_template('hw2.html', elem=elem)
 
 
 @app.route('/posts')
@@ -32,6 +43,5 @@ def posts():
 
 if __name__ == '__main__':
     constant = []
+    constant = create_lst()
     app.run(host="0.0.0.0", port=10000, debug=True)
-
-
