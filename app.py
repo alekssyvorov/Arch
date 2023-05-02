5e32b7292321dec63c58a36084b21f42e9717a9b
Djekson <alekssyvorov29@gmail.com>
2023-05-02 07:12:55 +0300

    V 1.4

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def index():
 
 
 class AddUser():
+    grade = 0
     def __init__(self, login, password1, password2, email):
         self.login = self.check_login(login)
         self.password1 = self.check_password(password1, password2)
@@ -27,24 +28,38 @@ class AddUser():
                 else:
                     flag = False
                     break
-        return flag
+        else:
+            print('Логин меньше 5 символов')
+        if flag:
+            return login
 
     def check_password(self, password1, password2):
         if len(password1) > 5:
             if password1 == password2:
-                return True
+                return password1
             else:
                 print('Пароли не совпадают')
         else:
             print('Пароль меньше 5 символов')
-            return False
+            raise ValueError
 
     def check_email(self, email):
         if email.count('@') == 1:
-            return True
+            return email
         else:
             print('Емейл не содержит @')
             return False
+
+    def check(self):
+        if self.login != False and self.password1 != False and self.email != False:
+            return True
+
+    def setGrade(self):
+        if len(self.password1) > 6:
+            from random import randint
+            self.grade = randint(0, 100)
+        return self.grade
+
 
 
 @app.route('/registration-user', methods=['POST', 'GET'])
@@ -57,21 +72,32 @@ def registration():
             email = request.form['email']
             user = AddUser(login, password1, password2, email)
             dataUser = {}
-            dataUser['login'] = login
-            dataUser['password'] = password1
-            dataUser['email'] = email
+            dataUser['login'] = user.login
+            dataUser['password'] = user.password1
+            dataUser['email'] = user.email
+            if user.setGrade():
+                dataUser['grade'] = user.grade
             print(dataUser)
+            if user.check():
+                return redirect('/select')
         except ValueError:
             print('Данные не получены от формы лого/пасс')
-
-
-
     return render_template('registration-user.html')
 
 
-@app.route('/posts')
-def posts():
-    return render_template('posts.html')
+@app.route('/select', methods=['POST', 'GET'])
+def select():
+    if request.method == 'POST':
+        try:
+            archives = request.form['archiv']
+            # С чек боксом не разобрался
+            # Если ставим галочку, то все ок
+            # А если не ставим, то падает программа... Надо как-то ошибку вызвать, пока не знаю как
+            checkbox_download = request.form['download']
+            print(archives, checkbox_download)
+        except ValueError:
+            print('что то пошло не хорошо')
+    return render_template('select.html')
 
 
 if __name__ == '__main__':
